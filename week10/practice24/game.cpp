9672ccedb3d13a30a666fd73ffafd4b47975aec6
derisan <derisan@naver.com>
2020-11-08 15:05:55 +0900

    실습 24 준비

---
+++ week10/practice24/game.cpp
@@ -0,0 +1,140 @@
+#include "game.h"
+
+#include <iostream>
+
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "renderer.h"
+#include "shader.h"
+#include "actor.h"
+
+Game::Game()
+	: mRenderer{ nullptr },
+	mMeshShader{ nullptr },
+	mScrWidth{ 0 },
+	mScrHeight{ 0 },
+	mShouldClose{ false },
+	mIsUpdating{ false }
+{
+
+}
+
+bool Game::Init(int* argc, char** argv, int w, int h)
+{
+	mScrWidth = w;
+	mScrHeight = h;
+
+	glutInit(argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+	glutInitWindowPosition(0, 0);
+	glutInitWindowSize(mScrWidth, mScrHeight);
+	glutCreateWindow("Practice 24");
+
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Unable to initialize GLEW" << std::endl;
+		return false;
+	}
+
+	mRenderer = Renderer::Get();
+	mMeshShader = mRenderer->GetShader("mesh");
+	mMeshShader->SetActive();
+	glm::mat4 view{ 1.0f };
+	view = glm::lookAt(glm::vec3{ 0.0f, 1.0f, 3.0f }, glm::vec3{ 0.0f, 0.0f, -1.0f }, glm::vec3{ 0.0f, 1.0f, 0.0f });
+	glm::mat4 proj{ 1.0f };
+	proj = glm::perspective(45.0f, static_cast<float>(mScrWidth) / mScrHeight, 0.1f, 100.0f);
+	mMeshShader->SetMatrix4Uniform("uView", view);
+	mMeshShader->SetMatrix4Uniform("uProj", proj);
+	
+	return true;
+}
+
+void Game::Run(unsigned char key)
+{
+	if (!mShouldClose)
+	{
+		ProcessInput(key);
+		Update();
+		Draw();
+	}
+}
+
+void Game::Shutdown()
+{
+	Renderer::Get()->Shutdown();
+
+	while (!mActors.empty())
+		delete mActors.back();
+}
+
+void Game::ProcessInput(unsigned char key)
+{
+	if (key == 0)
+		return;
+	else if (key == 27)
+		mShouldClose = true;
+	
+	for (auto actor : mActors)
+		actor->ProcessInput(key);
+}
+
+void Game::Update()
+{
+	std::vector<Actor*> deads;
+	mIsUpdating = true;
+	for (auto actor : mActors)
+	{
+		actor->Update();
+		if (actor->GetState() == Actor::State::kDead)
+			deads.emplace_back(actor);
+	}
+	mIsUpdating = false;
+
+	for (auto pending : mPendingActors)
+	{
+		pending->ComputeWorldTransform();
+		mActors.emplace_back(pending);
+	}
+	mPendingActors.clear();
+
+	for (auto actor : deads)
+	{
+		delete actor;
+	}
+}
+
+void Game::Draw()
+{
+	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glEnable(GL_DEPTH_TEST);
+
+	mMeshShader->SetActive();
+	for (auto actor : mActors)
+		actor->Draw(mMeshShader);
+
+	glutSwapBuffers();
+}
+
+void Game::AddActor(Actor* actor)
+{
+	if (mIsUpdating)
+		mPendingActors.emplace_back(actor);
+	else
+		mActors.emplace_back(actor);
+}
+
+void Game::RemoveActor(Actor* actor)
+{
+	auto iter = std::find(std::begin(mPendingActors), std::end(mPendingActors), actor);
+	if (iter != std::end(mPendingActors))
+		mPendingActors.erase(iter);
+
+	iter = std::find(std::begin(mActors), std::end(mActors), actor);
+	if (iter != std::end(mActors))
+		mActors.erase(iter);
+}
+
---
+++ week10/practice24/game.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <vector>
+
+class Game
+{
+public:
+	Game();
+
+	bool Init(int* argc, char** argv, int w = 1024, int h = 768);
+
+	void Run(unsigned char key);
+	void Shutdown();
+
+	void AddActor(class Actor* actor);
+	void RemoveActor(class Actor* actor);
+
+	// Getters
+	bool GetShouldClose() const { return mShouldClose; }
+	int GetScrWidth() const { return mScrWidth; }
+	int GetScrHeight() const { return mScrHeight; }
+
+	// Setters
+	void SetShouldClose(bool value) { mShouldClose = value; }
+
+private:
+	void ProcessInput(unsigned char key);
+	void Update();
+	void Draw();
+
+	std::vector<class Actor*> mActors;
+	std::vector<class Actor*> mPendingActors;
+
+	class Renderer* mRenderer;
+	class Shader* mMeshShader;
+
+	int mScrWidth;
+	int mScrHeight;
+
+	bool mShouldClose;
+	bool mIsUpdating;
+};
+
